5423c6d4e5fb0dd8b0a79c4ad697a6adf23a5e23
TSTZ <53590202+oukouwh@users.noreply.github.com>
2022-12-20 17:12:34 +0800

    [web_app] commit test

--- src/main/webapp/app/entities/enumerations/order-accuracy.model.ts
+++ src/main/webapp/app/entities/enumerations/order-accuracy.model.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: 王浩
+ * @Date: 2022-12-09 15:15:59
+ * @LastEditors: 王浩
+ * @LastEditTime: 2022-12-19 14:10:16
+ * @FilePath: /tiger_client/src/main/webapp/app/entities/enumerations/order-accuracy.model.ts
+ * @Description: 受注確度
+ */
 export enum OrderAccuracy {
   A = 'A',
 
