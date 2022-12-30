bfa2ebceac9cec0caa0346acdc6c9be8dcfa1e3e
TSTZ <53590202+oukouwh@users.noreply.github.com>
2022-12-30 11:52:03 +0800

    [web_app] modify拖拽追加

---
+++ src/main/webapp/app/auto-form/auto-form-hidden/auto-form-hidden.component.ts
@@ -0,0 +1,79 @@
+/*
+ * @Author: 王浩
+ * @Date: 2022-12-30 11:37:49
+ * @LastEditTime: 2022-12-30 11:42:16
+ * @FilePath: /tiger_client/src/main/webapp/app/auto-form/auto-form-hidden/auto-form-hidden.component.ts
+ * @Description: Do not edit
+ */
+/* eslint-disable @typescript-eslint/tslint/config */
+/* eslint-disable no-console */
+
+import { Component, OnInit, Input } from '@angular/core';
+import { field } from '../models/global.model';
+import { DndDropEvent, DropEffect } from 'ngx-drag-drop';
+import swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-auto-form-hidden',
+  templateUrl: './auto-form-hidden.component.html'
+})
+export class AutoFormHiddenComponent implements OnInit {
+  @Input() item?: field;
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  onDropSub(event: DndDropEvent, list?: any[]): void {
+    if (list && (event.dropEffect === 'copy' || event.dropEffect === 'move')) {
+      if (event.dropEffect === 'copy') event.data.fieldName = event.data.ctrlType + '-' + new Date().getTime();
+      let index = event.index;
+      if (typeof index === 'undefined') {
+        index = list.length;
+      }
+      if (list.length < 1) {
+        list.splice(index, 0, event.data);
+      }
+    }
+  }
+
+  removeField(i: number, list: any[]): void {
+    swal
+      .fire({
+        title: 'Are you sure?',
+        text: 'Do you want to remove this field?',
+        showCancelButton: true,
+        confirmButtonColor: '#00B96F',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, remove!'
+      })
+      .then(result => {
+        if (result.value) {
+          list.splice(i, 1);
+        }
+      });
+  }
+
+  onDragged(item: any, list: any[], effect: DropEffect) {
+    if (effect === 'move') {
+      const index = list.indexOf(item);
+      list.splice(index, 1);
+    }
+  }
+
+  onDragover(event: DragEvent): void {
+    console.log('dragover in Hidden', JSON.stringify(event, null, 2));
+  }
+
+  onDragStart(event: DragEvent): void {
+    console.log('drag started in hidden', JSON.stringify(event, null, 2));
+  }
+
+  releaseOp(event: DragEvent): void {
+    console.log('drag releaseOp in hidden', JSON.stringify(event, null, 2));
+  }
+
+  submit(): void { }
+
+  toggleValue(obj: any): void { }
+}
--- src/main/webapp/app/auto-form/auto-form.module.ts
+++ src/main/webapp/app/auto-form/auto-form.module.ts
@@ -1,11 +1,4 @@
-/*
- * @Author: 王浩
- * @Date: 2022-12-29 16:04:58
- * @LastEditors: 
- * @LastEditTime: 2022-12-29 16:16:55
- * @FilePath: /tiger_client/src/main/webapp/app/auto-form/auto-form.module.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
+import { AutoFormHiddenComponent } from './auto-form-hidden/auto-form-hidden.component';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'app/shared/shared.module';
 import { autoFormState } from './auto-form.route';
@@ -19,6 +12,8 @@ import { DndModule } from 'ngx-drag-drop';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { CalendarModule } from 'primeng/calendar';
+import { AutoFormComponent } from './main/auto-form.component';
+import { AutoFormConfigComponent } from './auto-form-config/auto-form-config.component';
 
 
 @NgModule({
@@ -36,7 +31,9 @@ import { CalendarModule } from 'primeng/calendar';
     DropdownModule
   ],
   declarations: [
-
+    AutoFormComponent,
+    AutoFormConfigComponent,
+    AutoFormHiddenComponent,
   ],
 })
 export class AutoFormModule { }
